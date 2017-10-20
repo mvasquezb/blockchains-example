71cbb4c71532b962d9bc7106173d7fcb6b726213
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-20 11:24:58 -0500

    Add example hashing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate eth_crypto;
+
+use eth_crypto::prelude::*;
+
+#[allow(dead_code)]
+fn example1() {
+    let mut a = [1, 2, 3, 4, 5];
+    let hash1 = hash(&a);
+    a[0] = 0;
+    let hash2 = hash(&a);
+    println!("hash-1: {:x}", &hash1);
+    println!("hash-2: {:x}", &hash2);
+}
+
 fn main() {
-    println!("Hello, world!");
+    example1();
 }
