87a3bb3033b819978620064083ae73029ea4149a
Martín Vásquez <pedromvasquezb@gmail.com>
2017-10-20 11:25:38 -0500

    Add example password verification with salt hashing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate eth_crypto;
 
 use eth_crypto::prelude::*;
+use eth_crypto::hash::keccak256::Hash;
 
 #[allow(dead_code)]
 fn example1() {
@@ -12,6 +13,23 @@ fn example1() {
     println!("hash-2: {:x}", &hash2);
 }
 
+fn verify(password: &str, challenge: &str, response: &Hash) -> bool {
+    let expected = hash_many(&[password, challenge]);
+    &expected == response
+}
+
+fn example2() {
+    let password = "decentralize all things";
+    let challenge = " where appropiate";
+    let response = hash_many(&[password, challenge]);
+    if verify(password, challenge, &response) {
+        println!("Welcome !");
+    } else {
+        println!("Access denied !");
+    }
+}
+
 fn main() {
-    example1();
+    // example1();
+    example2();
 }
